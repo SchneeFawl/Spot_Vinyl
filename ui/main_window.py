9a593d76f412d9a971fff5ee9abc84fd44bf7d95
SchneeFawl <175829050+SchneeFawl@users.noreply.github.com>
2026-05-03 23:50:45 +0530

    Add background image to main window

--- ui/main_window.py
+++ ui/main_window.py
@@ -1,5 +1,7 @@
+import sys
 from PyQt6.QtCore import Qt
-from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6.QtGui import QPixmap
+from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel
 
 
 class MainWindow(QMainWindow):
@@ -8,13 +10,19 @@ class MainWindow(QMainWindow):
 
         self.setWindowTitle("Spot_Vinyl")
         self.setFixedSize(500, 500)
-        self.setWindowFlags(Qt.WindowType.FramelessWindowHint)      # removes title bar
-        # self.setAttribute(Qt.WidgetAttribute.WA_TranslucentBackground)       # for transparency
+        self.setWindowFlags(Qt.WindowType.FramelessWindowHint)  # removes title bar
+        self.setAttribute(Qt.WidgetAttribute.WA_TranslucentBackground)    # for transparency
+
+        # the main background image
+        BG_LABEL = QLabel(self)
+        BG_LABEL.setGeometry(0, 0, 500, 500)
+        BACKGROUND = QPixmap("D:/Coding/Spot_Vinyl/assets/bg_main.png")
+        BG_LABEL.setPixmap(BACKGROUND)
+        BG_LABEL.setScaledContents(True)
 
 
 app = QApplication([])
-
 window = MainWindow()
 window.show()
 
-app.exec()
+sys.exit(app.exec())
