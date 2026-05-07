d95f049e3690ae5136a67b3b228681a458a5eedc
SchneeFawl <175829050+SchneeFawl@users.noreply.github.com>
2026-05-07 22:00:38 +0530

    Add thumbnail saver functionality

--- core/song.py
+++ core/song.py
@@ -1,16 +1,17 @@
 import asyncio
 from winrt.windows.media.control import \
     GlobalSystemMediaTransportControlsSessionManager as MediaManager
-from winrt.windows.storage.streams import \
-    DataReader, Buffer, InputStreamOptions
-# from winrt.windows.foundation import IAsyncOperation
+# from winrt.windows.storage.streams import \
+#    DataReader, Buffer, InputStreamOptions, IRandomAccessStream, IRandomAccessStreamReference
+from song_cover import thumbnail_saver as thumbnail_s
 
-async def spotify_info():
+async def song_info():
+    # song details
     sessions = await MediaManager.request_async()
     current_session = sessions.get_current_session()
     app_id = current_session.source_app_user_model_id.lower()
     TARGET_ID = "spotify"
-    # print(app_id)
+    #print(app_id)
 
     if current_session and TARGET_ID in app_id:
         info = await current_session.try_get_media_properties_async()
@@ -18,7 +19,10 @@ async def spotify_info():
         if info:
             title = info.title
             artist = info.artist
-            # print(title, artist)
-            return title, artist
+            thumbnail_info = info.thumbnail
+            thumbnail = await thumbnail_s(thumbnail_info)
+            #print(title, artist)
+            return title, artist, thumbnail
 
-asyncio.run(spotify_info())
+if __name__ == "__main__":
+    asyncio.run(song_info())
---
+++ core/song_cover.py
@@ -0,0 +1,31 @@
+import asyncio
+from winrt.windows.storage.streams import \
+    DataReader, IRandomAccessStreamReference, Buffer, InputStreamOptions
+from winrt.windows.media.control import \
+    GlobalSystemMediaTransportControlsSessionManager as MediaManager
+
+async def thumbnail_saver(thumbnail: IRandomAccessStreamReference):
+    # opening the stream buffer to get image data 
+    stream = await thumbnail.open_read_async()
+    buffer = Buffer(stream.size)
+    await stream.read_async(buffer, buffer.capacity, InputStreamOptions.NONE)
+
+    reader = DataReader.from_buffer(buffer)
+    thumb_bytes = bytearray(buffer.length)
+    byte_buffer = reader.read_bytes(thumb_bytes)
+
+    thumb_path = "core/song_cover.png"
+    with open(thumb_path, "wb") as file:
+        file.write(thumb_bytes)
+
+'''     # Calling the function in song.py
+async def song_cover_fetch():
+    manager = await MediaManager.request_async()
+    session = manager.get_current_session()
+    app_id = session.source_app_user_model_id.lower()
+
+    if "spotify" in app_id:
+        info = await session.try_get_media_properties_async()
+        await thumbnail_saver(info.thumbnail)
+'''
+# asyncio.run(song_cover_fetch())
