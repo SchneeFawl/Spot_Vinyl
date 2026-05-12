f38f82e657a9ad3ca82e03e8170c64918c97a60a
SchneeFawl <175829050+SchneeFawl@users.noreply.github.com>
2026-05-12 21:54:14 +0530

    Add new assets and add placeholders in settings menu

--- ui/settings_menu.py
+++ ui/settings_menu.py
@@ -1,4 +1,5 @@
-from PyQt6.QtWidgets import QWidget, QLabel, QPushButton
+from PyQt6.QtWidgets import \
+    QWidget, QLabel, QPushButton, QLineEdit, QCheckBox, QComboBox
 from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QPixmap
 from pathlib import Path
@@ -13,6 +14,7 @@ class SettingsMenu(QWidget):
         self.setFixedSize(500, 500)
         self.setAttribute(Qt.WidgetAttribute.WA_TranslucentBackground)
 
+        # bg
         self.bg_label = QLabel(self)
         self.bg_label.setGeometry(0, 0, 500, 500)
         bg_path = (assets_dir / "settings_menu.png") # type: ignore
@@ -20,7 +22,7 @@ class SettingsMenu(QWidget):
         self.bg_label.setPixmap(bg_img)
         self.bg_label.setScaledContents(True)
 
-        
+        # close button (settings)
         self.stng_close_btn = QPushButton(self)
         button_close_img = (assets_dir / "btn_close.png").as_posix() # type: ignore
         button_close_pressed_img = (assets_dir / "btn_close_pressed.png").as_posix() # type: ignore
@@ -39,4 +41,35 @@ class SettingsMenu(QWidget):
             }}
         """)
         self.stng_close_btn.setGeometry((85*5), (5*5), (10*5), (11*5))
-        
+
+        self.webhook_input = QLineEdit(self)
+        self.webhook_input.setPlaceholderText("Paste Discord Webhook URL here")
+        self.webhook_input.setGeometry((5*10), (20*5), (72*5), (8*5))
+
+        self.als_on_top = QCheckBox("Always on top", self)
+        self.als_on_top.setGeometry((10*5),(32*5), (40*5), (10*5))
+        als_on_top_img = (assets_dir / "on_top_checkbox.png").as_posix()   # type: ignore
+        als_on_top_checked_img = (assets_dir / "on_top_checkbox_checked.png").as_posix()   # type: ignore
+        self.als_on_top.setStyleSheet(f"""
+            QCheckBox::indicator {{
+                width: 50px;
+                height: 50px;
+            }}
+            QCheckBox::indicator:unchecked {{
+                image: url({als_on_top_img})
+                padding: 0px;
+            }}
+            QCheckBox::indicator:checked {{
+                background-image: url({als_on_top_checked_img});
+                padding: 0px;
+            }}
+        """)
+
+        self.theme_label = QLabel("Select theme:", self)
+        self.theme_label.setGeometry((10*5), (46*5), (20*5), (6*5))
+        self.theme_dropdown = QComboBox(self)
+        self.theme_dropdown.addItems(["Classic", "Space"])  # placeholder themes
+        self.theme_dropdown.setGeometry((10*5), (52*5), (40*5), (8*5))  # 48, 
+
+        self.test_discord_btn = QPushButton("Test Discord webhook", self)
+        self.test_discord_btn.setGeometry((10*5), (64*5), (40*5), (9*5))
